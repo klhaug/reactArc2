a1789a949f0926a6160e2509174e77dea9456df3
klhaug <kristian@snougle.no>
2025-08-08 12:27:19 +0200

    experimented on useRef hook with focus-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
 const [query, setQuery] = useState("")
 const [dogs, setDogs] = useState([]);
+const inputRef = useRef(null)
+
 
 
 useEffect(() => {
@@ -29,7 +31,7 @@ useEffect(() => {
       }
       fetchDogData()
 
-      }, 1000)
+      }, 200)
     
 
     return () => {
@@ -42,12 +44,20 @@ const handleSearch = (e) => {
   setQuery(e.target.value)
 }
 
+const handleOnClick = () => {
+  setQuery("")
+  inputRef.current.focus()
+  inputRef.current.value = ""
+}
+
+
   return (
     <>
       <h1>A page dedicated for practise. For repitition. For screwing up.</h1>
       <label htmlFor="search">Søk etter hunder</label><br/>
-      <input id="search" onChange={handleSearch} type="search"/>
+      <input ref={inputRef} id="search" onChange={handleSearch} type="search"/>
       <p>Your search: {query}</p>
+      <button onClick={handleOnClick}>Click me to focus the text input and clear the query!</button>
       {dogs.length > 0 ? dogs.map((dog) => 
       <div key={dog.id}>
         <h2>{dog.name}</h2>

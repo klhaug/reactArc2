43764e0c33f7c1742613a72a999d5f0989e9b202
klhaug <kristian@snougle.no>
2025-08-28 10:49:11 +0200

    put back my practise

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ export default function App() {
       </header>
       <main>
         
-        {/*<MobilityCalc /> */}
-        {/* <ToDo /> */}
-        {/* <DogSearch /> */}
+        <MobilityCalc />
+        <ToDo />
+        <DogSearch />
       </main>
     </>
   )
